0f731ae89327cfd8ad6cd21ea703f009988b14a2
Bozo-max <benjamin.fargeton@hotmail.fr>
2020-05-04 16:00:45 +0200

    Add template part

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
-    path('article/<int:id_article>', views.view_article),
+    path('home', views.home, name = 'home'),
+    path('article/<int:id_article>', views.view_article, name = 'display_article'),
+    path('date', views.current_date, name = 'current_date'),
+    path('add/<int:x>/<int:y>', views.addition, name = 'add')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
+from django.urls import reverse
+from datetime import datetime
 
 
 def home(request):
-    return HttpResponse('<h1>Hello World !</h1>')
+    return HttpResponse('<h1>Hello World !</h1><p>Vous etes sur %s </p>'%(reverse('home')))
+
+def current_date(request):
+    return render(request, 'blog/date.html.twig', {'date':datetime.now()})
+
+def addition(request, x, y):
+    res = x+y
+    return render(request, 'blog/addition.html.twig', locals())
 
 def view_article(request, id_article):
     if id_article >100:
         raise Http404
     elif id_article == 88:
-        return redirect('https://www.google.fr')
+        return redirect('home')
     return HttpResponse('<h1> Voici l\'article numéro %d !'%(id_article))
 # Create your views here.
