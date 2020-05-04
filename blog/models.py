e1b2ea60593d6a1f82565308c8339ded8c2833f8
Bozo-max <benjamin.fargeton@hotmail.fr>
2020-05-04 19:42:59 +0200

    database first part (article + categorie)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Article(models.Model):
+    titre = models.CharField(max_length = 255)
+    auteur = models.CharField(max_length = 255)
+    content = models.TextField(null = True)
+    date = models.DateTimeField(default = timezone.now, verbose_name = "Date de parution")
+    categorie = models.ForeignKey('Categorie', on_delete = models.CASCADE)
+
+    class Meta:
+        verbose_name = "un article"
+        ordering = ['date']
+
+        def __string__(self):
+            return self.titre
+
+class Categorie(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
